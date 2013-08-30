4c20be10a1be61f927cdc658f8af2182efc39367
rrnntt <rrnntt@ubuntu>
2013-08-30 11:19:05 +0100

    Added fpdf and fonts.

---
+++ font_view.py
@@ -0,0 +1,28 @@
+from fpdf import FPDF
+
+#font_file_name = 'font/mathjax_amsregular.ttf'
+font_file_name = 'font/MathJax/ttf/mathjax_size4regular.ttf' # large brackets and large square root
+
+size = 10
+pdf = FPDF()
+pdf.add_page()
+pdf.add_font('jax','',font_file_name,uni=True)
+
+lb = 0
+for n in range(0xeffc):
+    c = unichr(n)
+    pdf.set_font('jax','',size)
+    w = pdf.get_string_width(c)
+    if w > 0:
+        pdf.set_font('times','',size)
+        pdf.cell(13, 10, hex(n) + ': ' )
+        pdf.set_font('jax','',size)
+        pdf.cell(10, 10, c)
+        if lb == 6: 
+            pdf.ln(10)
+            lb = 0
+        else:
+            lb += 1
+    #print unichr(i)
+pdf.output('out/font_view.pdf', 'F')
+print 'done'
